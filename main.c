26773af471a4a3c9c24c83fd9ff82c8d72ce68f6
aiolos404 <aiolos404@gmail.com>
2015-09-09 19:10:13 -0400

    finished input arguments function

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
+// read the arguments from user's input. 
+// Only 2 arguments will be accepted.
+// Other arguments number will be rejected and cause "slient exit".
     int i;
     printf("Catch the arguments: ");
-    for(i=1; i<argc; i++) 
-    	printf("%s ", argv[i]);
+    if (argc == 3)
+    {
+   		printf("%s %s", argv[1], argv[2]);
+    }else{
+    	printf("slient exit");
+    } 
     printf("\n");
     return 0;
 } 
