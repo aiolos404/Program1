9fed595ee81c7ac3072f170946f342a9e6c59aa6
aiolos404 <aiolos404@gmail.com>
2015-09-09 19:18:12 -0400

    clean the project

--- get.c
+++ get.c
@@ -4,14 +4,13 @@ int main(int argc, char *argv[])
 // read the arguments from user's input. 
 // Only 2 arguments will be accepted.
 // Other arguments number will be rejected and cause "slient exit".
-    int i;
-    printf("Catch the arguments: ");
     if (argc == 3)
     {
-   		printf("%s %s", argv[1], argv[2]);
+   		printf("Catch the arguments: %s %s", argv[1], argv[2]);
     }else{
     	printf("slient exit");
     } 
     printf("\n");
     return 0;
 } 
+
