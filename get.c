a8fab9ae82df920d7736fee6ef51f1151572eb0b
aiolos404 <aiolos404@gmail.com>
2015-09-09 22:13:23 -0400

    Added readContentofFile function in the get class

--- get.c
+++ get.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 1024
+
+
+void readContentofFile(char *s)
+{
+    char buf[MAX_LINE];  //define the buffer area
+    FILE *f;            //set the pointer of file
+    int length;             //count the number of char per line
+    //check if the file exists
+    if((f = fopen(s,"r")) == NULL)
+    {
+        printf("no such file");
+        exit (1) ;
+    }
+    //print the content on the screen 
+    while(fgets(buf,MAX_LINE,f) != NULL)
+    {
+        length = strlen(buf);
+        buf[length-1] = '\0';  //remove '\0' tag
+        printf("%s \n",buf);
+    }
+}
+
+
+
 int main(int argc, char *argv[])
 {
 // read the arguments from user's input. 
@@ -9,8 +37,12 @@ int main(int argc, char *argv[])
    		printf("Catch the arguments: %s %s", argv[1], argv[2]);
     }else{
     	printf("slient exit");
+        exit (1) ;
     } 
     printf("\n");
+    readContentofFile(argv[1]);
     return 0;
 } 
+
+
 
